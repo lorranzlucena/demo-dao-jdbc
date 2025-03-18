75e51e446f7beb332998280f93dfc30bc8875c4e
lorranzlucena <lorranz.lucena.sousa@gmail.com>
2025-03-18 20:35:53 -0300

    Department class

---
+++ src/aplication/Program.java
@@ -0,0 +1,13 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department dep = new Department(1, "Books");
+
+	}
+
+}
